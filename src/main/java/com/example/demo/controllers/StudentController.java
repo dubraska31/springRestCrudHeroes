bbe75cb2066ec68d6dbf92ac441178a19dc889ef
dubra <dubra@DESKTOP-2DIMVDL.fibertel.com.ar>
2020-01-04 16:22:46 -0300

    refactor de paquetes

--- src/main/java/com/example/demo/controllers/StudentController.java
+++ src/main/java/com/example/demo/controllers/StudentController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.models.Student;
-import com.example.services.StudentService;
+import com.example.demo.services.StudentService;
 
 @RestController
 public class StudentController {
