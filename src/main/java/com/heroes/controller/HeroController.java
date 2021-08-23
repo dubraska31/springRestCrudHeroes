e01c58e91475f5ee372fa4ef68d6a00c1e470ddb
dubraska31 <dubraskaprimi31@gmail.com>
2021-08-23 00:25:45 -0300

    validacion delete

--- src/main/java/com/heroes/controller/HeroController.java
+++ src/main/java/com/heroes/controller/HeroController.java
@@ -3,12 +3,14 @@ package com.heroes.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.heroes.model.Hero;
@@ -20,32 +22,33 @@ public class HeroController {
 	@Autowired
 	HeroService heroService;
 
-	@GetMapping("/heroes")
+	@GetMapping("/hero")
 	public List<Hero> getAllHeroes() {
 		return heroService.getAllHeroes();
 	}
 
-	@GetMapping("/heroes/{idHero}")
+	@GetMapping("/hero/{idHero}")
 	public Hero getHeroById(@PathVariable long idHero) {
 		return heroService.getHeroById(idHero);
 	}
 
-	@PostMapping("/heroes")
+	@PostMapping("/hero")
 	public Hero saveHero(@RequestBody Hero hero) {
 		return heroService.saveHero(hero);
 	}
 
-	@PutMapping("/heroes")
+	@PutMapping("/hero")
 	public Hero updateHero(@RequestBody Hero hero) {
 		return heroService.updateHero(hero);
 	}
 
-	@DeleteMapping("/heroes/{idHero}")
-	public void deleteHero(@PathVariable long idHero) {
+	@DeleteMapping("/hero/{idHero}")
+	@ResponseStatus(HttpStatus.OK)
+	public void deleteHero(@PathVariable long idHero) throws Exception {
 		try {
 			heroService.deleteHero(idHero);
 		} catch (Exception e) {
-			System.out.println("Exception: " + e.getMessage());
+			throw new Exception(e);
 		}
 	}
 }
--- src/main/java/com/heroes/service/HeroService.java
+++ src/main/java/com/heroes/service/HeroService.java
@@ -3,7 +3,9 @@ package com.heroes.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.heroes.model.Hero;
 import com.heroes.repository.HeroRepository;
@@ -19,7 +21,7 @@ public class HeroService {
 	}
 
 	public Hero getHeroById(long idHero) {
-		return heroRepository.findById(idHero).get();
+		return heroRepository.findById(idHero).orElse(null);
 	}
 
 	public Hero saveHero(Hero hero) {
@@ -30,11 +32,19 @@ public class HeroService {
 		return heroRepository.save(hero);
 	}
 
-	public void deleteHero(long idHero) {
+	public void deleteHero(long idHero) throws ResponseStatusException, Exception {
 		try {
+			Hero hero = getHeroById(idHero);
+
+			if (hero == null) {
+				throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Hero not found");
+			}
+
 			heroRepository.deleteById(idHero);
+		} catch (ResponseStatusException e) {
+			throw new ResponseStatusException(e.getStatus(), e.getReason());
 		} catch (Exception e) {
-			System.out.println("Exception: " + e.getMessage());
+			throw new Exception(e);
 		}
 	}
 
